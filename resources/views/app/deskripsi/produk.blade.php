52a640e64698e20b6472024d906b1f6378a6d70b
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2020-05-26 16:11:20 +0700

    memperbaiki link bootstrap

--- resources/views/app/deskripsi/produk.blade.php
+++ resources/views/app/deskripsi/produk.blade.php
@@ -9,70 +9,78 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8">
-                <div class="col-md-12">
-                    <h3>Sukiman Rivero</h3>
-                </div>
-                <div class="row">
-                    <div class="col-md-6">
-                        <img src="{{asset('public/img/tukangkebun.png')}}" alt="">
-                    </div>
-                    <div class="col-md-6">
-                        <div class="col-md-12">
-                            <tr>
-                                <td>Harga</td>
-                                <td>10.000.000</td>
-                            </tr>
-                            <tr>
-                                <td>Jumlah</td>
-                                <td>2</td>
-                            </tr>
-                            <tr>
-                                <td>Metode Pengiriman</td>
-                                <td> 
-                                    <form action="/action_page.php">
-                                        <div class="form-check">
-                                            <label class="form-check-label" for="radio1">
-                                                <input type="radio" class="form-check-input" id="radio1" name="optradio" value="pengiriman-langsung" checked>Pengiriman Langsung
-                                            </label>
-                                        </div>
-                                        <div class="form-check">
-                                            <label class="form-check-label" for="radio2">
-                                                <input type="radio" class="form-check-input" id="radio2" name="optradio" value="ambil-di-toko">Ambil di Toko
-                                            </label>
-                                        </div>     
-                                    </form>
-                                </td>
-                            </tr>
+                <div class="card bitbit-card rounded-0 border border-light">
+                    <div class="container my-3">
+                        <div class="row pl-3">
+                            <div class="col-md-12">
+                                <h3>Sukiman Rivero</h3>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <img src="{{asset('public/img/tukangkebun.png')}}" alt="">
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="col-md-12">
+                                        <tr>
+                                            <td>Harga</td>
+                                            <td>10.000.000</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Jumlah</td>
+                                            <td>2</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Metode Pengiriman</td>
+                                            <td> 
+                                                <form action="/action_page.php">
+                                                    <div class="form-check">
+                                                        <label class="form-check-label" for="radio1">
+                                                            <input type="radio" class="form-check-input" id="radio1" name="optradio" value="pengiriman-langsung" checked>Pengiriman Langsung
+                                                        </label>
+                                                    </div>
+                                                    <div class="form-check">
+                                                        <label class="form-check-label" for="radio2">
+                                                            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="ambil-di-toko">Ambil di Toko
+                                                        </label>
+                                                    </div>     
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <p>
+                                            Lidah mertua atau mother in law;s tongue. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                        </p>
+                                    </div>
+                                   
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-md-12">
-                            <p>
-                                Lidah mertua atau mother in law;s tongue. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            </p>
-                        </div>
-                       
                     </div>
                 </div>
             </div>
 
             <div class="col-md-4">
-                <div class="card">
-                    <div class="card card-title">
-                        <h5><small>Dijual dan disediakan oleh</small></h5>
-                        <h3>Toko Bunga Kasih</h3>
-                    </div>
-                    <div class="rating">
-                        <span class="fa fa-star checked"></span>
-                        <span class="fa fa-star checked"></span>
-                        <span class="fa fa-star checked"></span>
-                        <span class="fa fa-star"></span>
-                        <span class="fa fa-star"></span>(30)
-                    </div>
-                    <div class="card card-title">
-                        <h5><small>Jakarta Utara</small></h5>
+                <div class="card bitbit-card rounded-0 border border-light">
+                    <div class="my-3">
+                        <div class="card-title">
+                            <h5><small>Dijual dan disediakan oleh</small></h5>
+                            <h3>Toko Bunga Kasih</h3>
+                        </div>
+                        <div class="rating">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>(30)
+                        </div>
+                        <div class="card card-title">
+                            <h5><small>Jakarta Utara</small></h5>
+                        </div>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="card card-title">
+                <div class="card card bitbit-card rounded-0 border border-light">
+                    <div class="card-title">
                         <h4>Metode Pengiriman</h4>
                         <h4>Lokasi</h4>
                         <h4>Sayang, Jatinangor, Kab. Sumedang, Jawa Barat Indonesia</h4>
@@ -112,22 +120,24 @@
 
             </div>
         </div>
-        <div class="card-block">
+        <div class="card bitbit-card rounded-0 border border-light card-block my-3">
             <div class="table">
                 <div class="col-md-12">
-                    <h3>Ulasan (30)</h3>
-                </div>
-                <div class="rating col-md-12">
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star"></span>
-                    <span class="fa fa-star"></span>(30)
+                    <h3 class="Ulasan my-3 pl-3">Ulasan (10)</h3>
                 </div>
                 <table class="table ppl-table ppl-table-striped ppl-table-separated">
+                    <tr>
+                        <td colspan="2">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>    4.9 <small>dari 5</small>
+                        </td>            
+                    </tr>
                     <tr class="">
-                        <td class="kz-profile-photo">
-                            <img src="{{url('public/img/tukangkebun.png')}}/img/photo-220-1.jpg" alt="" />
+                        <td class="bitbit-profile-photo">
+                            <img class="rounded-circle" src="{{asset('img/profileuser.png')}}" alt="" />
                         </td>
                         <td>
                             <h5>Rifaa Zalfaa<i class="kz-text-gold"></i></h5>
@@ -135,8 +145,8 @@
                         </td>
                     </tr>
                     <tr class="">
-                        <td class="kz-profile-photo">
-                            <img src="{{url('public/img/tukangkebun.png')}}/img/photo-220-1.jpg" alt="" />
+                        <td class="bitbit-profile-photo">
+                            <img class="rounded-circle" src="{{asset('img/profileuser.png')}}" alt="" />
                         </td>
                         <td>
                             <h5>Rifaa Zalfaa<i class="kz-text-gold"></i></h5>
@@ -144,8 +154,8 @@
                         </td>
                     </tr>
                     <tr class="">
-                        <td class="kz-profile-photo">
-                            <img src="{{url('public/img/tukangkebun.png')}}/img/photo-220-1.jpg" alt="" />
+                        <td class="bitbit-profile-photo">
+                            <img class="rounded-circle" src="{{asset('img/profileuser.png')}}" alt="" />
                         </td>
                         <td>
                             <h5>Rifaa Zalfaa<i class="kz-text-gold"></i></h5>
@@ -153,8 +163,8 @@
                         </td>
                     </tr>
                     <tr class="">
-                        <td class="kz-profile-photo">
-                            <img src="{{url('public/img/tukangkebun.png')}}/img/photo-220-1.jpg" alt="" />
+                        <td class="bitbit-profile-photo">
+                            <img class="rounded-circle" src="{{asset('img/profileuser.png')}}" alt="" />
                         </td>
                         <td>
                             <h5>Rifaa Zalfaa<i class="kz-text-gold"></i></h5>
--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -6,20 +6,21 @@
     <title>@yield('title')</title>
   <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     @yield('page-css')
-    <link rel="stylesheet" href="public/css/pejuang-custom.css">
+    <link rel="stylesheet" href="css/pejuang_custom.css" />
+    {{-- <link rel="stylesheet" href="{{asset('../css/pejuang-custom.css')}}"> --}}
 
     @yield('styles')
 
 
 </head>
-<body class="hold-transition sidebar-mini">
+<body class="hold-transition sidebar-mini" style="background: #fafafa">
     @include('layout.navbar')
     
     <div class="page-content">
