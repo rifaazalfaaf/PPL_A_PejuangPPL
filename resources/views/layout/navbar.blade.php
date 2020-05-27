9841b4330f564f3ba08c4b07cdab036c1f58f5ee
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2020-05-27 14:55:41 +0700

    memperbaiki navbar
    diberi logo, diberi garis vertikal, dan membuat navbar ketika berhasil login

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,32 +1,60 @@
-<div class="bitbit-navbar navbar navbar-expand-sm navbar-bitbit">
+<div class="bitbit-navbar navbar navbar-expand-sm fixed-top navbar-bitbit">
     <div class="container">
          <!-- Brand/logo -->
-        <a class="navbar-brand navbar-brand-bitbit col-md-1" href="/">BitBit</a>
+        <a class="navbar-brand col-md-1" href="/"><img class="logo-bitbit mb-2" src="{{asset('img/logo.png')}}" style="width: 80px;" alt=""></a>
         
         
-        <div class="input-group col-md-8 mb-3" style="margin-top: 10px;">
-            <input type="text" class="form-control" style="color: #6b8a3c" placeholder="Cari produk atau jasa...">
-                <div class="input-group-append">
-                    <button class="btn btn-success" type="submit">
-                        <i class="fa fa-search"></i>
+        <div class="input-group col-md-7 mb-3 p-0" style="margin-top: 10px;">
+            <input type="text" class="form-control" id="myInput" style="color: #6b8a3c" placeholder="Cari produk atau jasa disini...">
+                <div class="input-group-append" id="myDIV">
+                    <button class="btn border border-left-0" style="background: #ffffff;" type="submit">
+                        <i class="fa fa-search" style="color: #66990c"></i>
                     </button>
                 </div>
         </div>
         <!-- Links -->
-        <ul class="navbar-nav">
-            <li class="nav-item">
-               <a class="nav-link" href="{{url('/keranjang')}}">
-                    <i class="fa fa-shopping-cart fa-2x" href=""></i>
+        <ul class="navbar-nav col-md-4">
+            {{-- sebelum login --}}
+            {{-- <li class="nav-item">
+               <a class="nav-link px-3" href="{{url('/keranjang')}}">
+                    <i class="fas fa-shopping-cart mt-1" style="color: #548611;font-size:25px;" href=""></i>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{url('/login')}}">
-                    <button type="button" class="btn btn-light" style="background-color: #ffffff; color: #548611">Masuk</button>
+                <div class="garis_verikal mt-1 mr-3" style="border-left: 2px #548611 solid; height: 45px;width: 0px;"></div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link mr-1" href="{{url('/login')}}">
+                    <button type="button btn-login" class="btn" style="width: 120px; height: 40px;box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);background-color: #ffffff; color:#548611">Masuk</button>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="{{url('/register')}}">
-                    <button type="button" class="btn btn-success" style="background-color:  #66990c">Daftar</button>
+                    <button type="button" class="btn" style="width: 120px; height: 40px;box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);background-color: #66990c; color:#ffffff">Daftar</button>
+                </a>
+            </li> --}}
+            {{-- batas untuk sebelum login --}}
+
+            {{-- setelah login --}}
+            <li class="nav-item">
+               <a class="nav-link pl-3" href="{{url('/keranjang')}}">
+                    <i class="fas fa-shopping-cart mt-1" style="color: #548611;font-size:25px;" href=""></i>
+                </a>
+            </li>
+            <li class="nav-item pt-1 mr-3">
+                <a class="nav-link" href="{{url('/keranjang')}}">
+                    <label for="keranjang" style="font-size: 18px;color: #548611;font-family: Poppins;">Keranjangku</label>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link pt-2 mr-3" title="Pemberitahuan" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-content="isinya nanti pemberitahuan" style="color: #456c0a;" href="{{url('/pemberitahuan')}}">
+                    <i class="fas fa-bell mt-1" style="color: #548611;font-size:25px;"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{url('/profile')}}">
+                    <img class="rounded-circle" src="{{asset('img/profileuser.png')}}" style="width: 26.6px; height: 26px;" alt="">
+                    <label class="mt-1 pl-2" for="nama-user"  style="font-size: 18px;color: #548611;font-family: Poppins;">Agnes</label>
                 </a>
             </li>
         </ul>
