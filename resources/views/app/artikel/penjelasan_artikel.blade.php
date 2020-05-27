b02fcb1aefe8921206eb58e16ccce8b7f017e314
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2020-05-27 14:59:09 +0700

    membuat halaman artikel

---
+++ resources/views/app/artikel/penjelasan_artikel.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.default')
+@section('title','Penjelasan Artikel')
+
+@section('styles')
+ 
+@endsection
+
+@section('content')
+	<div class="container">
+		<div class="jumbotron rounded-0" style="background-image: url('img/Image19@2x.png')">
+		</div>
+		<div class="card container rounded-0 border border-light mb-3">
+			<div class="card-title nama_toko mt-3 pl-3">
+				Trik Merawat Tanaman yang Tepat Agar Tetap Subur
+			</div>
+			<div class="card-body deskripsi_tukang">
+				<p>Bagi kalian yang senang berkebun, aktivitas dan cara merawat tanaman bisa menjadi hobi yang paling asyik untuk dijalani. Bayangkan, dimulai dari membeli benih, menanam, hingga merawat, pasti ada kepuasan tersendiri dari cara merawat tanaman yang efektif hingga menghasilkan kebun yang subur dan berbunga wangi. Pasti senang dong jika kamu tidak perlu repot ke pasar karena ada tanaman menghasilkan di rumah?</p>
+				<p>Namun, apa jadinya bila hobi berkebun malah membawa stres karena tanaman sering layu dan tidak berkembang dengan baik. Frustrasi, sedih dan putus asa bisa saja membuat kamu meninggalkan hobi berkebun. Bisa jadi, cara merawat tanaman kamu salah. Makanya, kamu perlu tahu cara merawat tanaman yang baik supaya hobi berkebun tetap berjalan, manfaat kesehatan dan produksi juga tetap bisa kamu nikmati. Yuk simak langsung delapan cara merawat tanaman yang paling efektif bersama Kania!</p>
+
+1. Melakukan Teknik Penyiraman Air yang Tepat
+
+			</div>
+		</div>
+	</div>
+		
+	
+@endsection
