1693c57f0e9cc1841e551b7b2f87782fc70f0a58
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2020-05-27 14:58:11 +0700

    membuat halaman pemberitahuan

---
+++ app/Http/Controllers/PemberitahuanController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PemberitahuanController extends Controller
+{    
+    public function pemberitahuan()
+    {
+        return view('app/pemberitahuan/notif');
+    }
+	public function checkout()
+    {
+        return view('app/checkout/checkout');
+    }
+    
+}
---
+++ resources/views/app/pemberitahuan/notif.blade.php
@@ -0,0 +1,29 @@
+@extends('layout.default')
+@section('title','Notifikasi')
+
+@section('styles')
+ 
+@endsection
+
+@section('content')
+	<div class="container" style="min-height: 440px">
+		<div class="col-md-12">
+			<div class="card bitbit-card rounded-0 border border-light">
+				<div class="nama_toko card-title mt-2 pl-3">
+					Semua Pemberitahuan
+				</div>
+				<div class="row my-2 pl-3">
+					<div class="col-md-3">
+						<img src="img/lidahmertua.jpg" alt="">
+					</div>
+					<div class="col-md-9 mt-4">
+						<p class="pembeitahuan">Pesanan Sudah Diterima</p>
+						<p class="pembeitahuan">Paket 01283927463 telah diterima</p>
+						<p class="pembeitahuan">27-03-2020 14:22</p>
+					</div>
+				</div>
+			</div>
+		</div>
+			
+	</div>
+@endsection
