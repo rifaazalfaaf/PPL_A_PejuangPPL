d7b8866daad3e77ed7505a72898b9cc4288214a3
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2020-06-01 14:14:58 +0700

    mengubah tampilan tambah produk

--- resources/views/app/produk/tambahproduk.blade.php
+++ resources/views/app/produk/tambahproduk.blade.php
@@ -28,35 +28,61 @@
 					<form action="/action_page.php" class="p-3">
 				    	<div class="container">
 			            	<div class="form-group">
-						      	<label class="harga" for="namalengkap">Nama Lengkap</label>
-						      	<input type="text" class="form-control" id="namalengkap" name="namalengkap">
+						      	<label class="harga" for="namaproduk">Nama Produk</label>
+						      	<input type="text" class="form-control" id="namaproduk" name="namaproduk">
 						    </div>
-						    <div class="custom-control custom-radio custom-control-inline">
-						      	<input type="radio" class="custom-control-input" id="tukang_kebun" name="tukang_kebun">
-						      	<label class="custom-control-label harga" for="tukang_kebun">Tukang Kebun</label>
-						    </div>
-						    <div class="custom-control custom-radio custom-control-inline">
-						      	<input type="radio" class="custom-control-input" id="arsitektur" name="arsitektur">
-						      	<label class="custom-control-label harga" for="arsitektur">Arsitektur Taman</label>
-						    </div>
-							<div class="form-group">
-						      	<label class="harga" for="deskripsi">Deskripsi Jasa</label>
-						      	<input type="text" class="form-control" id="deskripsi" name="deskripsi">
-						    </div>
+
 						    <div class="form-group">
-						      	<label class="harga" for="gambar">Mahakarya</label><br>
+						      	<label class="harga" for="gambar">Gambar Produk</label><br>
 			            		<input type="file" class="form-control-file" id="gambar1" name="gambar1">
 			            		<input type="file" class="form-control-file" id="gambar2" name="gambar2">
 			            		<input type="file" class="form-control-file" id="gambar3" name="gambar3">
+						    </div>
+			            	
+			            	<div class="custom-control custom-radio custom-control-inline">
+						      	<input type="radio" class="custom-control-input" id="benih" name="benih">
+						      	<label class="custom-control-label harga" for="benih">Biji(Benih)</label>
+						    </div>
+						    <div class="custom-control custom-radio custom-control-inline">
+						      	<input type="radio" class="custom-control-input" id="gantung" name="gantung">
+						      	<label class="custom-control-label harga" for="gantung">Tanaman hias gantung</label>
+						    </div>
+						    <div class="custom-control custom-radio custom-control-inline">
+						      	<input type="radio" class="custom-control-input" id="buah" name="buah">
+						      	<label class="custom-control-label harga" for="buah">Tanaman hias buah</label>
+						    </div>
+						    <div class="custom-control custom-radio custom-control-inline">
+						      	<input type="radio" class="custom-control-input" id="bunga" name="bunga">
+						      	<label class="custom-control-label harga" for="bunga">Tanaman hias bunga</label>
+						    </div>
+						    <div class="custom-control custom-radio custom-control-inline">
+						      	<input type="radio" class="custom-control-input" id="pupuk" name="pupuk">
+						      	<label class="custom-control-label harga" for="pupuk">Pupuk</label>
+						    </div>
+						    <div class="custom-control custom-radio custom-control-inline">
+						      	<input type="radio" class="custom-control-input" id="air" name="air">
+						      	<label class="custom-control-label harga" for="air">Tanaman hias air</label>
+						    </div>
+						    <div class="custom-control custom-radio custom-control-inline">
+						      	<input type="radio" class="custom-control-input" id="mediatanam" name="mediatanam">
+						      	<label class="custom-control-label harga" for="mediatanam">Media Tanam</label>
+						    </div>
+						    <div class="custom-control custom-radio custom-control-inline">
+						      	<input type="radio" class="custom-control-input" id="hama" name="hama">
+						      	<label class="custom-control-label harga" for="hama">Pembasmi hama</label>
+						    </div>
+						    <div class="custom-control custom-radio custom-control-inline">
+						      	<input type="radio" class="custom-control-input" id="perkakas" name="perkakas">
+						      	<label class="custom-control-label harga" for="perkakas">Perkakas Kebun</label>
 						    </div>
 			            	<div class="form-group mt-2">
 						      	<label class="harga" for="harga">Harga Produk</label>
 						      	<input type="text" class="form-control" id="harga" name="harga">
 						    </div>
 				    	</div>
-						<div align="center">
+						<div class="col-md-2">
 			        		</label class="labell">
-			        		<input type="submit" value="Simpan dan Tampilkan" class="btn btn-beli">		
+			        		<input type="submit" value="Simpan" class="btn btn-beli">		
 			        	</div>
 			        </div>
 		    	</div>
